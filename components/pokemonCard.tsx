7760332c3f4484145c3285539bc5f9e87564c1a4
codingKermit <woojw6012@gmail.com>
2024-10-31 17:41:15 +0900

    component update

--- components/pokemonCard.tsx
+++ components/pokemonCard.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import Link from "next/link";
-import { Card, CardLink } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import TypeButton from "./TypeButton";
 
 const getPokemon = async (url) => {
