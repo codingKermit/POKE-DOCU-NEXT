16bb10adbd77d96227c9702da2fc458e7d1d6bd1
codingKermit <woojw6012@gmail.com>
2024-11-01 11:23:17 +0900

    .

--- components/TypeButton.tsx
+++ components/TypeButton.tsx
@@ -2,8 +2,6 @@ import '../styles/global.css';
 import typeButton from '../styles/TypeButton.module.css'
 
 export default function TypeButton({type, text}){
-    console.log('type :',type);
-    console.log('text :',text);
     return(
         <div>
             <span className={`${typeButton[type]} ${typeButton.typeBtn}`}>
